5b145e08f6611396d723c4a59b26714e3cf6032f
Shoregill <80356831+Shoregill@users.noreply.github.com>
2022-10-17 10:11:30 +0100

    first commit

---
+++ challenge2.js
@@ -0,0 +1,7 @@
+const person = {
+    firstName:"John",
+    lastName:"Doe",
+    favouriteColour:"Sky blue"
+};
+
+console log.('My name is $(firstName) $(lastName) and my favourite colour is $(favouriteColour)')
---
+++ fizzbang.js
@@ -0,0 +1,11 @@
+let num = 15
+
+if(num % 5 == 0 && num % 3 == 0){
+    console.log("Fizz Buzz")
+} else if(num % 5 == 0){
+    console.log("Buzz")
+} else if(num % 3 == 0){
+    console.log("Fizz")
+}else{
+    console.log(num +   "..booooo...num.toExponential.")
+}
